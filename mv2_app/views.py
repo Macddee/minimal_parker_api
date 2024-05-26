a6fa03af388c1ceeffa07010ffb1e03b9561500a
Macdonald Manana <macddeemanana@gmail.com>
2024-05-26 10:01:47 +0200

    Update views.py

--- mv2_app/views.py
+++ mv2_app/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
-from django.shortcuts import get_object_or_404
 from django.forms import model_to_dict
 from django.core.exceptions import ObjectDoesNotExist
 
